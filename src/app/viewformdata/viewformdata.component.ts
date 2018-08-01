53a58306f0b05be3fa724a854c8aac42a9fe8abc
unknown <rohitgoel280@gmail.com>
2018-08-01 09:34:10 +0530

    final changes

--- src/app/viewformdata/viewformdata.component.ts
+++ src/app/viewformdata/viewformdata.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-viewformdata',
@@ -7,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewformdataComponent implements OnInit {
   show;
-  constructor() {
+  constructor(private routes:Router) {
     this.show = JSON.parse(localStorage.getItem("totaldata"));
   }
 
   ngOnInit() {
   }
-
+  backtoedit() {
+    this.routes.navigate(["/editdata"]);
+  }
 }
